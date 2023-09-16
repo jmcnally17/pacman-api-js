26ef4ab4925a68dc909a926821ef519ef2bb1e16
Joe McNally <joe.t.mcnally@hotmail.com>
2023-09-17 00:51:09 +0100

    Update port number

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const scoresRouter = require("./routes/scores");
 
 const app = express();
 
-const url = process.env.REACT_APP_URL || "http://localhost:3000";
+const url = process.env.REACT_APP_URL || "http://localhost:8000";
 
 app.use(logger("dev"));
 app.use(express.json());
